10796354d848a342cc85ff5a7d4464b8136ff998
Cracklitosss <marroquinca17@gmail.com>
2024-01-21 21:01:34 -0600

    Primer commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,22 +7,17 @@ const messageRoutes = require('./routes/messageRoutes');
 
 const app = express();
 
-// Conexión a MongoDB
 mongoose.connect(process.env.DB_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Middleware para parsear el cuerpo de las solicitudes
 app.use(express.json());
 
-// Rutas de Usuarios
-app.use('/api', userRoutes);
 
-// Rutas de Mensajes
+app.use('/api', userRoutes);
 app.use('/api', messageRoutes);
 
-// Puerto de escucha
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor en ejecución en el puerto ${PORT}`);
